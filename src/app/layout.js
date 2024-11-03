3915135058bba95645fe7d89c5d388474f6e5c4b
warabist <sarutyinbanko@gmail.com>
2024-11-03 12:13:41 +0900

    よりわかりやすいファイル名に変更、ちょっとスマホ対応

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,9 +3,6 @@ import '@/globals.css';
 import iconImage from '@/icon.png';
 
 export const metadata = {
-    title: {
-        template: '%s | WM-ADDON-BOX'
-    },
     icons: {
         icon: iconImage.src,
         shortcut: iconImage.src,
@@ -14,25 +11,15 @@ export const metadata = {
 };
 
 export default function GlobalLayout({ children }) {
-
     return (
         <html>
             <body>
                 <header>
                     <img src={iconImage.src} alt=''></img>
                     <h1>WM-ADDON-BOX</h1>
-                    <ul>
-                        <li>
-                            <Link href='/' className='header-list-item'>ホーム</Link>
-                            <Link href='https://x.com/motiwarabist' className='header-list-item'>X</Link>
-                            <Link href='https://discord.gg/ZyrbU3GkAW' className='header-list-item'>Discord</Link>
-                        </li>
-                    </ul>
                 </header>
-                <hr size='2' color='darkgray' style={{ marginTop: 0 }}></hr>
                 {children}
             </body>
         </html>
     );
-
 }
